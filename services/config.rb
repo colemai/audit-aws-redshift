90dce847012db6d3ad86358a68819c2edb3c98b7
Ian Coleman <colemai@tcd.ie>
2017-01-03 14:50:55 -0800

    test3

--- services/config.rb
+++ services/config.rb
@@ -86,7 +86,7 @@ coreo_aws_advisor_alert "redshift-no-user-logging" do
   description "Redshift database user logging is disabled."
   category "Audit"
   suggested_action "Enable Redshift database user logging."
-  level "Warning"
+  level "Alert"
   objectives ["clusters", "logging_status"]
   call_modifiers [{}, {:cluster_identifier => "clusters.cluster_identifier"}]
 #   id_map "modifiers.parameter_group_name"
