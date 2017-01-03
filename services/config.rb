7aa8055713956c5281c13ffa35f275ba35621d88
Ian Coleman <colemai@tcd.ie>
2017-01-03 14:40:54 -0800

    test2

--- services/config.rb
+++ services/config.rb
@@ -92,7 +92,7 @@ coreo_aws_advisor_alert "redshift-no-user-logging" do
 #   id_map "modifiers.parameter_group_name"
   audit_objects ["", "logging_enabled"]
   operators ["", "=="]
-  alert_when ["", false]
+  alert_when ["", true]
 end
 
 # coreo_aws_advisor_alert "redshift-no-connection-logging" do
